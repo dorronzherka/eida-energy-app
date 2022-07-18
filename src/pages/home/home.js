f8ded33ac4e46121c8dc130b561fcb4130cdd5b4
Dorron Zherka <dorronzherka@Dorrons-MacBook-Pro.local>
2022-07-18 16:01:24 +0200

    hotfix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -15,8 +15,8 @@ export default function Home(prop) {
                     <BoxInfo value={parseFloat(location.consumption_total)} type='kWh' title='Total Consumption'/>
                     <BoxInfo value={parseFloat(location.daily_prod_KWH)} type='kWh' title='Total Production Today'/>
                     <BoxInfo value={Math.ceil(parseFloat(location.daily_prod_KWH) * 7)} type='kWh' title='Total Production Week'/>
-                    <BoxInfo value={location.monthly_prod_KWH != null ?  parseFloat(location.monthly_prod_KWH) : parseFloat(location.daily_prod_KWH) * 30} type='kWh' title='Total Production Month'/>
-                    <BoxInfo value={location.yearly_prod_KWH != null ? parseFloat(location.yearly_prod_KWH) : parseFloat(location.daily_prod_KWH) * 365} type='kWh' title='Total Production Year '/>
+                    <BoxInfo value={location.monthly_prod_KWH != null ?  parseFloat(location.monthly_prod_KWH) : Math.ceil(parseFloat(location.daily_prod_KWH) * 30)} type='kWh' title='Total Production Month'/>
+                    <BoxInfo value={location.yearly_prod_KWH != null ? parseFloat(location.yearly_prod_KWH) : Math.ceil(parseFloat(location.daily_prod_KWH) * 365)} type='kWh' title='Total Production Year '/>
                     <BoxInfo value={Math.floor(parseFloat(location.consumption_total) * daysLeftThisMonth)} type='kWh' title='Forecasted consumption'/>
                     <BoxInfo value={Math.ceil(parseFloat(location.daily_prod_KWH) * daysLeftThisMonth)} type='kWh' title='Forecasted production kwh'/>
                     <BoxInfo value={parseFloat(location.total_sold_PV)} type='kWh/NOK' title='Forecasted production kwh'/>
